5c17baff8b40b6d3be7756f909ed9f1515a46862
EditChar <kopazar10@hotmail.com>
2023-07-15 16:48:32 +0300

    backBtn

--- app/src/main/java/com/example/materialdesigndash/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/materialdesigndash/Activities/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.example.materialdesigndash.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
@@ -8,9 +9,15 @@ import com.example.materialdesigndash.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private ConstraintLayout backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        backBtn=findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(v -> finish());
+
     }
 }
